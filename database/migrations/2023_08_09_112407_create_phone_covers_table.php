2ac5a85555638c9fffbf29c3afadd04df56b6e4b
Ali Choopani <ali.choopani1320@gmail.com>
2023-08-09 14:56:42 +0330

    v 0.32 DB enhancement

---
+++ database/migrations/2023_08_09_112407_create_phone_covers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phone_covers', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand')->nullable();
+            $table->string('name');
+            $table->string('phone_model');
+            $table->enum('color', ['White', 'Black', 'Purple', 'Red', 'Blue', 'Yellow', 'Pink', 'Gray', 'Green'])->nullable();
+            $table->unsignedBigInteger('price');
+            $table->boolean('approved')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phone_covers');
+    }
+};
