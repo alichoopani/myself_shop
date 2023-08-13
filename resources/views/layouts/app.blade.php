8682e0ec315964007d8c5563b5c7aac7884094b5
Ali Choopani <ali.choopani1320@gmail.com>
2023-08-13 11:52:18 +0330

    v 0.59 livewire

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,8 @@
 
                 <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @livewireStyles
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -82,5 +84,7 @@
                 });
             });
         </script>
+        @livewireScripts
+
     </body>
 </html>
