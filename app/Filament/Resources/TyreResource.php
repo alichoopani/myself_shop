bb2adaad36599c02745e823ef2b2f05b05174590
Ali Choopani <ali.choopani1320@gmail.com>
2023-08-24 16:13:09 +0330

    v 0.85

--- app/Filament/Resources/TyreResource.php
+++ app/Filament/Resources/TyreResource.php
@@ -30,6 +30,12 @@ class TyreResource extends Resource
                 Forms\Components\TextInput::make('structure')->label('Structure')->required()->default('Radial'),
                 Forms\Components\TextInput::make('load_index')->label('Load Index')->required(),
                 Forms\Components\DatePicker::make('create_week')->required()->label('Create Week'),
+                Forms\Components\TextInput::make('size')->required()->label('Size')->numeric()->default(15),
+                Forms\Components\TextInput::make('weight')->label('Weight(Kg)')->required()->default(2.5)->numeric(),
+                Forms\Components\TextInput::make('width')->label('Width')->required()->default(255)->numeric(),
+                Forms\Components\TextInput::make('grip')->label('Grip')->required()->default(400)->numeric(),
+                Forms\Components\TextInput::make('top_speed_rating')->label('Top Speed Rating')->required()->default('H'),
+                Forms\Components\Checkbox::make('approved')->label('Approved')->default(1)
             ]);
     }
 
@@ -37,7 +43,13 @@ class TyreResource extends Resource
     {
         return $table
             ->columns([
-                //
+                Tables\Columns\TextColumn::make('brand')->label('Brand'),
+                Tables\Columns\TextColumn::make('name')->label('Name'),
+                Tables\Columns\TextColumn::make('speed_rating')->label('Speed Rating'),
+                Tables\Columns\TextColumn::make('size')->label('Size'),
+                Tables\Columns\TextColumn::make('width')->label('Width'),
+                Tables\Columns\TextColumn::make('grip')->label('Grip'),
+                Tables\Columns\CheckboxColumn::make('approved')->label('Approved')
             ])
             ->filters([
                 //
