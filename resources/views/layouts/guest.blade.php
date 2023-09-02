1e266dcdb50272d45c1571730145fc17652a8510
Ali Choopani <ali.choopani1320@gmail.com>
2023-09-02 10:13:33 +0330

    v 1.031

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -34,7 +34,7 @@
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
         @include('layouts.navigation')
-                <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden sm:rounded-lg">
+                <div class="w-full mt-36 px-12 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden ">
                 {{ $slot }}
                 </div>
         @include('layouts.footer')
