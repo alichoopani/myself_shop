27021fc11820b81e46d3d40e8768b5ed9443d89d
Ali Choopani <ali.choopani1320@gmail.com>
2023-09-02 10:20:39 +0330

    v 1.031

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,6 +12,13 @@
                 <x-input-error :messages="$errors->get('name')" class="mt-2"/>
             </div>
 
+        <!-- Phone -->
+            <div class="mt-4">
+                <x-input-label for="name" :value="__('Phone')"/>
+                <x-text-input id="name" class="block mt-1 w-full" type="number" name="phone" :value="old('phone')" required autocomplete="phone"/>
+                <x-input-error :messages="$errors->get('phone')" class="mt-2"/>
+            </div>
+
             <!-- Email Address -->
             <div class="mt-4">
                 <x-input-label for="email" :value="__('Email')"/>
