9530dfb11acdd07ada01be30a4d83210309bead3
Ali Choopani <ali.choopani1320@gmail.com>
2023-09-05 14:40:50 +0330

    v 1.1

--- database/migrations/2023_09_05_100504_create_order_transactions_table.php
+++ database/migrations/2023_09_05_100504_create_order_transactions_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('order_transactions', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('order_id')->nullable();
+            $table->decimal('final_price', 9, 0)->nullable();
+            $table->string('bank_name')->nullable();
+            $table->string('bank_recv')->nullable();
+            $table->mediumText('bank_send')->nullable();
+            $table->mediumText('bank_log')->nullable();
+            $table->enum('status', [
+                'pending',
+                'confirm',
+                'canceled'
+            ]);
             $table->timestamps();
         });
     }
