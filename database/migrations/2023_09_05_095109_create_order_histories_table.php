b84ddeb63ecf9ef1d5e66b9d77272dd1bdf5dc84
Ali Choopani <ali.choopani1320@gmail.com>
2023-09-05 13:23:20 +0330

    v 1.1

---
+++ database/migrations/2023_09_05_095109_create_order_histories_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_histories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('order_id');
+            $table->text('note');
+            $table->enum('status', [
+                'payment-pending',
+                'payment-confirm',
+                'payment-cancel',
+                'refund-pending',
+                'refund-confirm',
+                'processing',
+                'add-to-cart',
+                'delivery-started',
+                'delivered',
+                'canceled-by-admin',
+                'canceled-by-seller',
+                'canceled-by-customer'
+            ]);
+            $table->boolean('approved')->default(1);
+            $table->timestamps();
+            $table->timestamps('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_histories');
+    }
+};
