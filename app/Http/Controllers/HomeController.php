2b5e6f794a9f1e4c1edfa67f8611288d20237112
Ali Choopani <ali.choopani1320@gmail.com>
2023-09-05 15:02:41 +0330

    v 1.1

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,32 +15,9 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $tShirts = TShirt::query()
-            ->where('approved', 1)
-            ->orderBy('created_at', 'desc')
-            ->get();
-
-        $latestHats = Hat::query()
-            ->where('approved', 1)
-            ->orderBy('created_at', 'desc')
-            ->get();
-
-        $accessories = PhoneCover::query()
-            ->where('approved', 1)
-            ->orderBy('created_at', 'desc')
-            ->inRandomOrder()
-            ->get();
-
-        $latestBags = Bag::query()
-            ->where('approved', 1)
-            ->orderBy('created_at', 'desc')
-            ->get();
 
         return view('index', [
-            'tShirts' => $tShirts,
-            'latestHats' => $latestHats,
-            'accessories' => $accessories,
-            'latestBags' => $latestBags
+
         ]);
     }
 }
